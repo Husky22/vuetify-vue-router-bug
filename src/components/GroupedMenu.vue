1adc8db27bbdbe4afc7859eb8c80533a93a95901
Niklas von Minckwitz <niklas.v.minckwitz@googlemail.com>
2024-08-15 20:55:46 +0200

    init

---
+++ src/components/GroupedMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-btn>
+    Click Me
+    <v-menu activator="parent">
+      <v-list>
+        <v-list-group v-for="(entry, _idx) in props.entries">
+          <template #activator="{ props }">
+            <v-list-item v-bind="props" :title="entry.name"></v-list-item>
+          </template>
+          <v-list-item :to="entry.subEntry.to">{{
+            entry.subEntry.name
+          }}</v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-menu>
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+import { RouteLocationRaw } from "vue-router";
+
+type Entry = {
+  name: string;
+  value: string;
+  subEntry: {
+    name: string;
+    value: string;
+    to: RouteLocationRaw;
+  };
+};
+
+const props = defineProps<{ entries: Entry[] }>();
+</script>
+
---
+++ src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <GroupedMenu :entries="entries"/>
+</template>
+
+<script lang="ts" setup>
+  const entries = [
+  {name: "Entry 1",
+  value: "Entry 1",
+  subEntry: {
+name: "Sub 1",
+value: "Sub 1",
+to: {query: {val: "queryVal"}}
+  }},
+  {name: "Entry 2",
+  value: "Entry 2",
+  subEntry: {
+name: "Sub 2",
+value: "Sub 2",
+to: {query: {val: "queryVal"}}
+  }}
+  ]
+</script>
